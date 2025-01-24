7176677ac17a20628fa415a747f7d4fe1c599371
amosomondi <amosckie@gmail.com>
2025-01-24 11:36:17 +0300

    refined some more

--- fronten/src/App.js
+++ fronten/src/App.js
@@ -1,14 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './components/dashboard'
 import Login from './components/Login';
-import AdminLogin from './components/AdminLogin';
-import AdminDashboard from './components/AdminDashboard';
 import Register from './components/Register';
-import Header from './components/Header';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import ProductCard from './components/product';
-import ProductList from './components/productList';
 import React, { useState } from 'react';
 
 
@@ -28,9 +23,8 @@ function App(){
         <Routes>
       <Route path='/' element={<Dashboard />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/adminLogin' element={<AdminLogin />} />
       <Route path='/register' element={<Register />} />
-      <Route path='/products' element={<ProductList />} />
+      
       </Routes>
 
       </div>
